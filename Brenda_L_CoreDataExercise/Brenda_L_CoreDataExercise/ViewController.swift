56a742286e85674bf11e965bdb3093a62ee2edae
labrendis <67968260+labrendis@users.noreply.github.com>
2020-08-25 09:47:46 -0500

    Add files via upload

---
+++ Brenda_L_CoreDataExercise/Brenda_L_CoreDataExercise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Brenda_L_CoreDataExercise
+//
+//  Created by Brenda Leyva on 8/20/20.
+//  Copyright © 2020 Brenda Leyva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
